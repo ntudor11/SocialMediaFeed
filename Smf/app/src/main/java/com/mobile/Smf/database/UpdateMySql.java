ac969dd3cf9068d43678180734dbfff97b3cc36e
Thomas Høpfner-Dahl <thomashopfner-dahl@Thomass-MacBook-Pro.local>
2019-04-15 17:46:52 +0200

    database stuff

---
+++ Smf/app/src/main/java/com/mobile/Smf/database/UpdateMySql.java
@@ -0,0 +1,35 @@
+package com.mobile.Smf;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.concurrent.Callable;
+
+public class UpdateMySql implements Callable<Boolean> {
+
+    private String DB_URL, user, pass, instruction;
+
+    public UpdateMySql(String DB_URL, String user, String pass, String sqlInstruction) {
+
+        this.DB_URL = DB_URL;
+        this.user = user;
+        this.pass = pass;
+        this.instruction = sqlInstruction;
+    }
+
+    public Boolean call() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection(DB_URL, user, pass);
+            System.out.println("Database connection success UpdateMySql");
+
+            Statement st = con.createStatement();
+
+            int eval = st.executeUpdate(instruction);
+            //eval can be used for evaluation if needed
+        } catch (ClassNotFoundException e) { e.printStackTrace(); return false;}
+          catch (SQLException ex) { ex.printStackTrace(); return false; }
+          return true;
+    }
+}
