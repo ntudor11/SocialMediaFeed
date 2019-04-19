06bb1951a9e6c4f02e416c9e165f28721ca237fe
pezh <pezh@itu.dk>
2019-04-19 12:33:09 +0200

    started styling, added MakeTextPostFragment

--- Smf/app/src/main/java/com/mobile/Smf/fragments/MakeTextPostFragment.java
+++ Smf/app/src/main/java/com/mobile/Smf/fragments/MakeTextPostFragment.java
@@ -1,7 +1,65 @@
 package com.mobile.Smf.fragments;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.mobile.Smf.R;
+import com.mobile.Smf.activities.FeedActivity;
+import com.mobile.Smf.database.DataInterface;
+import com.mobile.Smf.model.User;
 
 public class MakeTextPostFragment extends Fragment {
 
+    private TextView textViewHeader;
+    private EditText editTextInputText;
+    private Button buttonUploadNewPost;
+
+    private DataInterface dataInterface;
+    private User user;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View makePostView = inflater.inflate(R.layout.fragment_maketextpost,container,false);
+
+        dataInterface = new DataInterface(getContext());
+        user = dataInterface.getLoggedInUser();
+
+        textViewHeader = makePostView.findViewById(R.id.maketextpost_textview_header);
+        editTextInputText = makePostView.findViewById(R.id.maketextpost_edittext_text);
+        buttonUploadNewPost = makePostView.findViewById(R.id.maketextpost_button_uploadnewpostbutton);
+
+        textViewHeader.setText(R.string.maketextpost_header);
+        buttonUploadNewPost.setText(R.string.maketextpost_uploadbutton);
+
+        buttonUploadNewPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = editTextInputText.getText().toString();
+
+                // upload the new post and respond accordingly
+                if (dataInterface.uploadTextPost(user.getUserName(),text)){
+                    // go back to feed after posting
+                    Intent intent = new Intent(getContext(), FeedActivity.class);
+                } else {
+                    // else make a toast and let user try again?
+                    Toast.makeText(getContext(),"Could not upload post, try again.",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        return makePostView;
+    }
 }
