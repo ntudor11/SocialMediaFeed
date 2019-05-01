39c751dcc753e405b52032694f4914300d122ddc
Zander Havgaard <pezh@itu.dk>
2019-05-01 14:54:38 +0200

    added much nicer overscroll events and triggering for fetching posts

--- Smf/app/src/main/java/com/mobile/Smf/fragments/FeedFragment.java
+++ Smf/app/src/main/java/com/mobile/Smf/fragments/FeedFragment.java
@@ -20,7 +20,15 @@ import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
 
+import me.everything.android.ui.overscroll.IOverScrollDecor;
+import me.everything.android.ui.overscroll.IOverScrollStateListener;
+import me.everything.android.ui.overscroll.OverScrollDecoratorHelper;
+
 import static com.mobile.Smf.model.Feed.getFeedSingleton;
+import static me.everything.android.ui.overscroll.IOverScrollState.STATE_BOUNCE_BACK;
+import static me.everything.android.ui.overscroll.IOverScrollState.STATE_DRAG_END_SIDE;
+import static me.everything.android.ui.overscroll.IOverScrollState.STATE_DRAG_START_SIDE;
+import static me.everything.android.ui.overscroll.IOverScrollState.STATE_IDLE;
 
 public class FeedFragment extends Fragment implements Observer {
 
@@ -58,6 +66,33 @@ public class FeedFragment extends Fragment implements Observer {
     private void startRecyclerView() {
         postRecyclerViewAdapter = new PostRecyclerViewAdapter(feed.getFeedAsList());
         feedRecyclerView.setAdapter(postRecyclerViewAdapter);
+
+        OverScrollDecoratorHelper.setUpOverScroll(feedRecyclerView, OverScrollDecoratorHelper.ORIENTATION_VERTICAL);
+
+        // Note: over-scroll is set-up using the helper method.
+        IOverScrollDecor decor = OverScrollDecoratorHelper.setUpOverScroll(feedRecyclerView, OverScrollDecoratorHelper.ORIENTATION_VERTICAL);
+
+        decor.setOverScrollStateListener(new IOverScrollStateListener() {
+                                             @Override
+                                             public void onOverScrollStateChange(IOverScrollDecor decor, int oldState, int newState) {
+                                                 switch (newState) {
+                                                     case STATE_IDLE:
+                                                         break;
+                                                     case STATE_DRAG_START_SIDE:
+                                                         getNewerPostsEvent();
+                                                         break;
+                                                     case STATE_DRAG_END_SIDE:
+                                                         getOlderPostsEvent();
+                                                         break;
+                                                     case STATE_BOUNCE_BACK:
+                                                         if (oldState == STATE_DRAG_START_SIDE) {
+                                                         } else {
+                                                         }
+                                                         break;
+                                                 }
+                                             }
+                                         });
+
 
         feedRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
             @Override
@@ -99,7 +134,7 @@ public class FeedFragment extends Fragment implements Observer {
     }
 
     private void getNewerPostsEvent(){
-        Toast.makeText(getContext(),"Getting newer posts...",Toast.LENGTH_SHORT).show();
+        Toast.makeText(getContext(),"Looking for newer posts...",Toast.LENGTH_SHORT).show();
         feed.updateWithNewerPosts();
         postRecyclerViewAdapter.notifyDataSetChanged();
 //        feed.updateWithOlderPosts();
@@ -107,7 +142,7 @@ public class FeedFragment extends Fragment implements Observer {
     }
 
     private void getOlderPostsEvent(){
-        Toast.makeText(getContext(),"Getting older posts...",Toast.LENGTH_SHORT).show();
+        Toast.makeText(getContext(),"Looking for older posts...",Toast.LENGTH_SHORT).show();
         feed.updateWithOlderPosts();
         postRecyclerViewAdapter.notifyDataSetChanged();
 //        feed.updateWithOlderPosts();Toast.makeText(getContext(),"",Toast.LENGTH_LONG).show();
